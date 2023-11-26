39ac135f070330285a39fdb5f323a74da6ac5939
thatsBruno <brunoandreribeiro18@gmail.com>
2023-11-26 21:18:53 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './styles.scss';
+
+//Get the button
+var mybutton = document.getElementById("topButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+
+  mybutton.addEventListener("click", topFunction);
